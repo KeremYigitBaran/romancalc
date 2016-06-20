3677d9c31e638bf25f4c9bd5f213094bee6dbd25
Butch Howard <butchhoward@gmail.com>
2016-06-20 01:41:48 -0400

    Reform looking patterns

--- romanmath.c
+++ romanmath.c
@@ -1,46 +1,69 @@
 #include "romanmath.h"
 #include <string.h>
 
+int romanSymbolToInt( const char RomanSymbol )
+{
+	int value = 0;
+
+	switch (RomanSymbol)
+	{
+	case 'I':
+		value = 1;
+		break;
+	case 'V':
+		value = 5;
+		break;
+	case 'X':
+		value = 10;
+		break;
+	default:
+		value = 0;
+		break;
+	};
+
+	return value;
+}
+
 int romanToInt( const char* Roman)
 {
 	if (strcmp(Roman, "I") == 0)
 	{
-		return 1;
+		return romanSymbolToInt('I');
 	}
 	else if (strcmp(Roman, "II") == 0)
 	{
-		return 2;
+		return romanSymbolToInt('I') + romanSymbolToInt('I');
 	}
 	else if (strcmp(Roman, "III") == 0)
 	{
-		return 3;
+		return romanSymbolToInt('I') + romanSymbolToInt('I') + romanSymbolToInt('I');
 	}
 	else if (strcmp(Roman, "IV") == 0)
 	{
-		return 4;
+		return romanSymbolToInt('V') - romanSymbolToInt('I');
 	}
 	else if (strcmp(Roman, "V") == 0)
 	{
-		return 5;
+		return romanSymbolToInt('V');
 	}
 	else if (strcmp(Roman, "VI") == 0)
 	{
-		return 6;
+		return romanSymbolToInt('V') + romanSymbolToInt('I');
 	}
 	else if (strcmp(Roman, "VII") == 0)
 	{
-		return 7;
+		return romanSymbolToInt('V') + romanSymbolToInt('I') + romanSymbolToInt('I');
 	}
 	else if (strcmp(Roman, "VIII") == 0)
 	{
-		return 8;
+		return romanSymbolToInt('V') + romanSymbolToInt('I') + romanSymbolToInt('I') + romanSymbolToInt('I');
 	}
 	else if (strcmp(Roman, "IX") == 0)
 	{
-		return 9;
+		return romanSymbolToInt('X') - romanSymbolToInt('I');
 	}
 	else
 	{
-		return 10;
+		return romanSymbolToInt('X');
 	}
 }
