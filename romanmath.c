770672e406474adb76b323d6ecf1acd919305452
Butch Howard <butchhoward@gmail.com>
2016-06-20 02:02:16 -0400

    Convert LCDM

--- romanmath.c
+++ romanmath.c
@@ -16,6 +16,18 @@ int romanSymbolToInt( const char RomanSymbol )
 	case 'X':
 		value = 10;
 		break;
+	case 'L':
+		value = 50;
+		break;
+	case 'C':
+		value = 100;
+		break;
+	case 'D':
+		value = 500;
+		break;
+	case 'M':
+		value = 1000;
+		break;
 	default:
 		value = 0;
 		break;
