5e32d309f6d5b251af5dd4de67c71b0107abdfee
Butch Howard <butchhoward@gmail.com>
2016-06-20 02:27:17 -0400

    Check some invalid values

--- romanmath.c
+++ romanmath.c
@@ -36,14 +36,60 @@ int romanSymbolToInt( const char RomanSymbol )
 	return value;
 }
 
+int romanAllowedSymbolRepeat(int value)
+{
+	int repeatCountAllowed = 0;
+	switch(value)
+	{
+	case 1:
+	case 10:
+	case 100:
+		repeatCountAllowed = 3;
+		break;
+	case 5:
+	case 50:
+	case 500:
+		repeatCountAllowed = 2;
+		break;
+	case 1000:
+		repeatCountAllowed = 4;
+		break;
+	default: 
+		repeatCountAllowed = 0;
+	}
+
+	return repeatCountAllowed;
+}
+
 int romanToInt( const char* Roman)
 {
 	int value = 0;
 	int previousSymbolValue = 0;
+	int repeatCount = 0;
 
 	for (int index = strlen(Roman)-1; index >=0; --index)
 	{
 		int symbolValue = romanSymbolToInt(Roman[index]);
+		if (symbolValue == 0)
+		{
+			value = 0;
+			break;
+		}
+		
+		if (symbolValue == previousSymbolValue)
+		{
+			++repeatCount;
+			if (repeatCount > romanAllowedSymbolRepeat(symbolValue))
+			{
+				value = 0;
+				break;
+			}
+		}
+		else
+		{
+			repeatCount = 1;
+		}
+
 		if (symbolValue < previousSymbolValue)
 		{
 			value -= symbolValue;
